2c2290a7d9a960062a9a4519581db5b2eaa47204
Dan Printzell <xwildn00bx@gmail.com>
2013-01-09 19:48:43 +0100

    Added missing include Signed-off-by: Dan Printzell <xwildn00bx@gmail.com>

--- src/nbtsave.h
+++ src/nbtsave.h
@@ -1,6 +1,7 @@
 #ifndef NBTSAVE_H
 #define NBTSAVE_H
 
+#include <stdint.h>
 void nbt_save_map(const char * filename, char dimension, char scale, int16_t height, int16_t width, int64_t xCenter, int64_t zCenter, unsigned char * mapdata);
 void nbt_load_map(const char * filename, unsigned char * mapdata);
 void save_raw_map(const char * filename, unsigned char * mapdata);
